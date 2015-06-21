68759e314a7dbceb4aea29a98fea00f1549f3189
Ricardo Fleury <ricardofleury@Ricardos-MacBook-Pro.local>
2015-06-20 22:51:50 -0500

    Add main menu

--- src/main.lua
+++ src/main.lua
@@ -6,7 +6,11 @@ function get_abstract(arg)
 end
 
 function invalid()
-    channel.say("No dice")
+    channel.say("Not a valid selection")
+end
+
+function select_category(cat)
+	selected_category = cat
 end
 
 local http = require 'summit.http'
@@ -15,8 +19,32 @@ local speech = require 'summit.speech'
 local json = require 'json'
 local inspect = require "inspect"
 
-local res, err = http.get('http://getnewspeak.herokuapp.com/headlines')
+channel.answer()
+channel.say('Welcome to Newspeak')
 
+-- Main menu
+selected_category = ""
+local category_names = { 'u.s.', 'world', 'entertainment', 'sports', 'business', 'politics', 'technology', 'science', 'health' }
+local cat_counter = 1
+local main_menu = menu()
+
+main_menu.intro("Select a news topic.")
+for index, category in ipairs(category_names) do 
+	main_menu.add(tostring(index), "For " .. category .. ", press " .. tostring(index), function() select_category(category) end)
+end
+main_menu.run()
+
+channel.say("Active Category: " .. selected_category)
+
+
+
+
+
+-- param passed back (category name)
+
+local url = "http://getnewspeak.herokuapp.com/headlines?categories=" .. selected_category
+repl()
+local res, err = http.get(url)
 
 if err then
     channel.say('Unable to contact news service, goodbye.')
@@ -25,20 +53,20 @@ end
 
 local news_content = json:decode(res.content)
 
-channel.answer()
-channel.say('Welcome to Newspeak')
 
-local my_ivr = menu()
 
-my_ivr.add(tostring(1), "Press " .. tostring(1) .. " for " .. news_content[1].title, function() get_abstract(news_content[1]) end)
-my_ivr.add(tostring(2), "Press " .. tostring(2) .. " for " .. news_content[2].title, function() get_abstract(news_content[2]) end)
-my_ivr.add(tostring(3), "Press " .. tostring(3) .. " for " .. news_content[3].title, function() get_abstract(news_content[3]) end)
-my_ivr.add(tostring(4), "Press " .. tostring(4) .. " for " .. news_content[4].title, function() get_abstract(news_content[4]) end)
-my_ivr.add(tostring(5), "Press " .. tostring(5) .. " for " .. news_content[5].title, function() get_abstract(news_content[5]) end)
-my_ivr.default(invalid)
-my_ivr.intro("Make your selection, please")
 
-local selection = my_ivr.run()
+local category_menu = menu()
+
+category_menu.add(tostring(1), "Press " .. tostring(1) .. " for " .. news_content[1].title, function() get_abstract(news_content[1]) end)
+category_menu.add(tostring(2), "Press " .. tostring(2) .. " for " .. news_content[2].title, function() get_abstract(news_content[2]) end)
+category_menu.add(tostring(3), "Press " .. tostring(3) .. " for " .. news_content[3].title, function() get_abstract(news_content[3]) end)
+category_menu.add(tostring(4), "Press " .. tostring(4) .. " for " .. news_content[4].title, function() get_abstract(news_content[4]) end)
+category_menu.add(tostring(5), "Press " .. tostring(5) .. " for " .. news_content[5].title, function() get_abstract(news_content[5]) end)
+category_menu.default(invalid)
+category_menu.intro("Make your selection, please")
+
+local selection = category_menu.run()
 
 channel.say("Goodbye, bitches!")
 
