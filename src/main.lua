fecea74caed688499f34b3d040979ab0a4bf6281
Ricardo Fleury <ricardofleury@Ricardos-MacBook-Pro.local>
2015-06-20 23:55:55 -0500

    Abstract out into functions

--- src/main.lua
+++ src/main.lua
@@ -1,8 +1,15 @@
 -- This comment enforces unit-test coverage for this file:
 -- coverage: 0
 
+http = require 'summit.http'
+menu = require 'summit.menu'
+speech = require 'summit.speech'
+json = require 'json'
+inspect = require "inspect"
+
 function get_abstract(arg)
 	channel.say(arg.abstract)
+	get_link_menu(arg.url)
 end
 
 function invalid()
@@ -13,65 +20,81 @@ function select_category(cat)
 	selected_category = cat
 end
 
-local http = require 'summit.http'
-local menu = require 'summit.menu'
-local speech = require 'summit.speech'
-local json = require 'json'
-local inspect = require "inspect"
+function send_article(selection)
+	local url = selection
+	repl()
+	-- Not implemented
+end
 
-local phone_number = channel.data.ani
-channel.say(phone_number)
+function hangup()
+	channel.say("Goodbye, bitches!")
+	channel.hangup()
+end
 
 
-channel.answer()
-channel.say('Welcome to Newspeak')
+phone_number = channel.data.ani
+
 
 -- Main menu
-selected_category = ""
-local category_names = { 'u.s.', 'world', 'entertainment', 'sports', 'business', 'politics', 'technology', 'science', 'health' }
-local cat_counter = 1
-local main_menu = menu()
-
-main_menu.intro("Select a news topic.")
-for index, category in ipairs(category_names) do 
-	main_menu.add(tostring(index), "For " .. category .. ", press " .. tostring(index), function() select_category(category) end)
-end
-main_menu.run()
-
-channel.say("Selected Category: " .. selected_category)
-
-
-
-
-
--- param passed back (category name)
-
-local url = "http://getnewspeak.herokuapp.com/headlines?categories=" .. selected_category
-local res, err = http.get(url)
-
-if err then
-    channel.say('Unable to contact news service, goodbye.')
-    channel.hangup()
+function get_main_menu()
+	selected_category = ""
+	category_names = { 'u.s.', 'world', 'sports', 'business', 'technology', 'science', 'health' }
+	cat_counter = 1
+	main_menu = menu()
+	main_menu.intro("Select a news topic.")
+	for index, category in ipairs(category_names) do 
+		main_menu.add(tostring(index), "For " .. category .. ", press " .. tostring(index), function() select_category(category) end)
+	end
+	main_menu.run()
+	channel.say("Selected Category: " .. selected_category)
+	get_category_menu()
 end
 
-local news_content = json:decode(res.content)
+
+-- API Call
+function get_category_menu()
+	url = "http://getnewspeak.herokuapp.com/headlines?categories=" .. selected_category
+	res, err = http.get(url)
+
+	if err then
+	    channel.say('Unable to contact news service, goodbye.')
+	    channel.hangup()
+	end
+
+	news_content = json:decode(res.content)
+
+	-- Category Menu
+	category_menu = menu()
+
+	category_menu.add(tostring(1), "Press " .. tostring(1) .. " for " .. news_content[1].title, function() get_abstract(news_content[1]) end)
+	category_menu.add(tostring(2), "Press " .. tostring(2) .. " for " .. news_content[2].title, function() get_abstract(news_content[2]) end)
+	category_menu.add(tostring(3), "Press " .. tostring(3) .. " for " .. news_content[3].title, function() get_abstract(news_content[3]) end)
+	category_menu.add(tostring(4), "Press " .. tostring(4) .. " for " .. news_content[4].title, function() get_abstract(news_content[4]) end)
+	category_menu.add(tostring(5), "Press " .. tostring(5) .. " for " .. news_content[5].title, function() get_abstract(news_content[5]) end)
+	category_menu.default(invalid)
+	category_menu.intro("Make your selection, please")
+
+	category_menu.run()
+	get_link_menu()
+end
 
 
+-- Link Menu
+function get_link_menu(selection)
+	link_menu = menu()
+
+	link_menu.add(tostring(1), "To receive a link to the full article, press 1", function() send_article(selection) end)
+	link_menu.add(tostring(2), "To continue browsing, press 2", get_main_menu)
+	link_menu.add(tostring(3), "To close Newspeak, press 3", hangup)
+	link_menu.default(invalid)
+	link_menu.intro("Make your selection, please")
+	link_menu.run()
+end
+
+-- Welcome
+channel.answer()
+channel.say('Welcome to Newspeak')
+get_main_menu()
 
 
-local category_menu = menu()
-
-category_menu.add(tostring(1), "Press " .. tostring(1) .. " for " .. news_content[1].title, function() get_abstract(news_content[1]) end)
-category_menu.add(tostring(2), "Press " .. tostring(2) .. " for " .. news_content[2].title, function() get_abstract(news_content[2]) end)
-category_menu.add(tostring(3), "Press " .. tostring(3) .. " for " .. news_content[3].title, function() get_abstract(news_content[3]) end)
-category_menu.add(tostring(4), "Press " .. tostring(4) .. " for " .. news_content[4].title, function() get_abstract(news_content[4]) end)
-category_menu.add(tostring(5), "Press " .. tostring(5) .. " for " .. news_content[5].title, function() get_abstract(news_content[5]) end)
-category_menu.default(invalid)
-category_menu.intro("Make your selection, please")
-
-local selection = category_menu.run()
-
-channel.say("Goodbye, bitches!")
-
-channel.hangup()
 
